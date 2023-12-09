351a0582c4ccd8877247f0f99429dbc88809c1e0
wee_ping_kuah <kuah_wee_ping@tech.gov.sg>
2023-12-09 19:10:37 +0800

    feat: completed day 9

---
+++ day9/day9-1.py
@@ -0,0 +1,38 @@
+file_path = 'day9.txt'
+
+def allZeros(arr):
+    return all(element == 0 for element in arr)
+
+def getNextDiffArray(currentDiffArray):
+    nextDiffArray = []
+    for i in range(len(currentDiffArray) - 1):
+        diff = currentDiffArray[i+1] - currentDiffArray[i]
+        nextDiffArray.append(diff)
+    return nextDiffArray
+
+def predictHistoryValue(historyArray):
+    diffArrayCollector = []
+    currentDiffArray = historyArray
+    while (True):
+        nextDiffArray = getNextDiffArray(currentDiffArray)
+        diffArrayCollector.append(nextDiffArray)
+        if (allZeros(nextDiffArray)):
+            break
+        currentDiffArray = nextDiffArray
+    lastValue = 0
+    for diffArray in reversed(diffArrayCollector):
+        lastValue += diffArray[-1]
+    return lastValue + historyArray[-1]
+
+
+
+with open(file_path, 'r') as file:
+
+    sumValue = 0 
+    
+    for line in file:
+        historyArray = [int(value) for value in line.split()]
+        historyValue = predictHistoryValue(historyArray)
+        sumValue += historyValue
+    
+    print(sumValue)
